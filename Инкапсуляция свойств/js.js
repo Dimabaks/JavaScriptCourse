4230dd8b4665067493d0bfbcf75088d86ae7150c
Dima <dimonbacsa@gmail.com>
2025-09-25 15:32:31 +0300

    update 25.09.2025

--- Инкапсуляция свойств/js.js
+++ Инкапсуляция свойств/js.js
@@ -1,17 +1,33 @@
 "use strict";
 
 class Account {
-    movements = [];
+    #movements = [];
+    #pin
     constructor(owner, currency, pin) {
         this.owner = owner;
         this.currency = currency;
-        this.pin = pin;
+        this.#pin = pin;
         console.log(`Вы создали аккаунт ${this.owner}`)
     }
     sayHi() {
         console.log("Hello world");
     }
+    changePin(password) {
+        this._pin = password;
+    }
+    deposit(val) {
+        this.#movements.push(val);
+    }
+    withdraw(val) {
+        this.deposit(-val);
+    }
+    getMove() {
+        return this.#movements;
+    }
 }
 
 const ivan = new Account("Ivan", "RUB", 1111);
+ivan.deposit(3000);
+ivan.deposit(400);
+ivan.withdraw(100);
 console.log(ivan);
